111783f458ab48f07fafbeda1fdeb206e98b7911
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-20 16:17:16 -0700

    print _putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,17 @@
 /**
  *main - entry point of our prg
  *
- *Return= 0 if success
+ *Return: 0 if success
  */
 int main(void)
 {
 print_putchar();
 return (0);
 }
-
+/**
+ *print_putchar - print _putchar followed by  new line
+ *
+ */
 void print_putchar(void)
 {
 putchar('_');
