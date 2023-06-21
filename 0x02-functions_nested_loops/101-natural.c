aad758676daeb416f814a221eab2be8c54af8026
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-20 19:39:44 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 	int a = 0;
+
 	for (i = 1; i < 1024; i++)
 	{
-		if ((i % 3 == 0) ||(i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 			a += i;
 	}
 	printf("%d\n", a);
-
 	return (0);
 }
 
