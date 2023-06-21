53afd5ced4a02e42c0f3fce69c1047162b3a0886
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-20 19:32:18 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the sum of all mult of 5 and 3 below 1024
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	int a = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) ||(i % 5 == 0))
+			a += i;
+	}
+	printf("%d\n", a);
+
+	return (0);
+}
+
+
