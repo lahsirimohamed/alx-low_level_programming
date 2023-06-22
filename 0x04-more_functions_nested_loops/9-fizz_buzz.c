80536eb909bc1b97d8d4ef20d0e98a3ec256bbcf
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-22 10:17:11 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, fizz for 3 multip
+ * buzz for 5 multip, and fizzbuzz for 3 and 5 multip
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && !(i % 5 == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if (!(i % 3 == 0) && (i % 5 == 0))
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			_putchar(i + '0');
+			_putchar(' ');
+		}
+	}
+	return (0);
+}
