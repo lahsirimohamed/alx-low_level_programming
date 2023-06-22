a9a488091984d77c7a1bba2385e256b5cba72ab9
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-22 10:52:52 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,23 @@
 
 int main(void)
 {
-	int i, a, b;
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz");
-		}
-		if (i % 3 != 0 && i % 5 == 0)
-		{
-			printf("Buzz");
-		}
 		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		if ((i % 3 != 0) && (i % 5 != 0))
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
 		{
 			printf("%d", i);
 		}
