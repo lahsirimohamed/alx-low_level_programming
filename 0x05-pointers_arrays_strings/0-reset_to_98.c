8e352a6afcb92518b4078b9804f2131551a2a154
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 16:19:05 -0700

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - takes a pointer to an int as parameter and update
+ * reset_to_98 - takes a pointer to an int as parameter and update
  * @n: pointer to an int
  * Return: (void) no return value
  */
