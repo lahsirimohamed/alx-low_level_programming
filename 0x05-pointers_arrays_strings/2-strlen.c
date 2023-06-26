07ca0a2b5bd3aa155b4beadc47dd9bbda78e2033
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 16:44:53 -0700

    lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen-the length of a string
+ * @s:string to test
+ * Return: lenght of a string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+	a = sizeof(*s);
+	return a;
+}
+
