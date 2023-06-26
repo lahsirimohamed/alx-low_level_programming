e8aaf0322c80a90ca5453055bd412f911b079116
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 16:12:14 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - takes a pointer to an int as parameter and update
+ * @n: pointer to an int
+ * Return: (void) no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
