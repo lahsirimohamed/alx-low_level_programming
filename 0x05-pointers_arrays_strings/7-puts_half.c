fe0d93ee0210afb1e6f8f43e871c55bf2e64d130
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 18:32:30 -0700

    half of dtring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half -prints half of a string, followed by a new line.
+ * @str: string to print
+ * Return: (void) no return value
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	for (i = len; i >= len / 2 + 1; len--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
