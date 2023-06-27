7531a704e0779d6e3514a5abfeb63d57204bef9d
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 18:05:38 -0700

    reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: string to rerverse
+ * Return: (void) no return value.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	char *f = s;
+	char *e = s;
+	char mid;
+
+	while (*e != '\0')
+	{
+		len++;
+		end++;
+	}
+
+	end--;
+
+	while (f < e)
+	{
+		mid = *f;
+		*f = *e;
+		*e = mid;
+		f++;
+		e--;
+	}
+}
