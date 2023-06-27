140549904d00be7bdfd632129bdc0205654edc4d
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 17:48:28 -0700

    prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	while (*s != 0)
+	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
-	while (len >= 0)
+	while (len > 0)
 	{
-		_putchar(*s[len]);
+		_putchar(*s);
 		len--;
 	}
 	_putchar('\n');
