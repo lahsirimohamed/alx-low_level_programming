07be5967b0244c360c94e82632d29aa46d849b3b
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-26 19:02:57 -0700

    half of dtring

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy-copies the string pointed to by src
+ * @dest:  point to buffer
+ * @src: string to copy
+ * Return:  the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
