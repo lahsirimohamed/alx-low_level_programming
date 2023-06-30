055161e7f7dcb6612250e4fbd1af34a0d294eae4
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-29 19:07:18 -0700

    push

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string-apitalizes all words of a string
+ * @n: pointer to a string
+ * Return: pointer to a strin
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i] != 0)
+	{
+		while (n[i] < 'a' || n[i] > 'z')
+			i++;
+		if (n[i - 1] == ' ' || n[i - 1] == '      ' || n[i - 1] == '\n' ||
+				n[i - 1] == ',' || n[i - 1] == ';' ||
+				n[i - 1] == '.' || n[i - 1] == '!' || n[i - 1] == '?' ||
+				n[i - 1] == '"' || n[i - 1] == '(' || n[i - 1] == ')' ||
+				n[i - 1] == '{' || n[i - 1] == '}')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
