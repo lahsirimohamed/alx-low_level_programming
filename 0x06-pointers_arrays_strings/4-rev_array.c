eb84c0c73e88fbdd3426adab9e69d8cdb9f85bbe
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-06-29 18:08:47 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array-everses the content of an array of integers.
+ * @a: input 
+ * @n: number of elements in the array
+ * Return: (void) no return value
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = tmp;
+	}
+}
