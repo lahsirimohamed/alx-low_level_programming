be05b843657f565010f370a90757e9ce19946045
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-05 16:07:32 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,6 +12,13 @@ int is_palindrome(char *s)
 	return (palind(s, 0));
 }
 
+/**
+ * palind-test if the string is palindrome
+ * @s: string
+ * @i: string char
+ * Return: 0 if not, 1 if palindrome
+ */
+
 int palind(char *s, int i)
 {
 	if (*s)
@@ -31,3 +38,16 @@ int palind(char *s, int i)
 		return (1);
 }
 
+/**
+ * _strlen_recursion-calclate the length of a string
+ * @s: string
+ * Return: the length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
