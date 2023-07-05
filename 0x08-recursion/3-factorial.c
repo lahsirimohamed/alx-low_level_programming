612f9d3d54f3f802c5ef181292ffcec209bb6b12
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-05 12:27:38 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,8 +11,10 @@ int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	if (n < 0)
+	else if (n < 0)
 		return (-1);
-	if (n > 0)
-		return (n * factorial(n - 1));
+	else if (n > 0)
+	{
+		int m = factorial(n - 1);
+		return (n * m);
 }
