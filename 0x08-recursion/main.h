4338a84fb91003f55c41b44a7627f4c92d0a828c
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-05 13:38:01 -0700

    _pow_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,13 @@
 
 void _puts_recursion(char *s);
 int _putchar(char c);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
 
 
 
