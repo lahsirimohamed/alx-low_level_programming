11251dd2b50458512c87b49f6f5de5028ca9bb84
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-07 16:46:46 -0700

    add nums

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (isnum(argv[i]) == 1)
+			{
+				sum += convert(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
+
+/**
+ * isnum-check if s contain a specific char
+ * @s: string to check
+ * Return:1 if nmber 0 else
+ */
+
+int isnum(char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * convert-convert a string to integer
+ * @s: string to convert
+ * Return: the integer
+ */
+
+int convert(char *s)
+{
+	int num = 0;
+	int sign = 1;
+	int d = 0;
+
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		d = *s - '0';
+		num = num * 10 + d;
+		s++;
+	}
+
+	num = sign * num;
+	return (num);
+}
+
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int isnum(char *s);
 int convert(char *s);
 
 #endif
