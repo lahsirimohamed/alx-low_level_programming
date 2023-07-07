399bacd2441249c09d7dd9d19d6886ef4d66605a
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-07 14:58:21 -0700

    print name prg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main-print name of the prg
+ * Return-void no return value
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s/n", argv[0]);
+	return (0);
+}
