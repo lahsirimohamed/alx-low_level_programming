fd87c6c11da6564d8e8afdb5adf728fd0ac4874a
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-07 19:07:09 -0700

    coints

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,9 +4,9 @@
 
 /**
  * main-prints the minimum number of coins to make change
- * @argc: number of arg
- * @argv: array of arg
- * @Return: 0 id succes
+ * @argc:number of arg
+ * @argv:array of arg
+ * Return:0 if success
  */
 
 
@@ -22,17 +22,21 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (cents <= 0)
+	else
 	{
-		printf("%d\n", 0);
+		if (cents <= 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		for (i = 0; i < 5; i++)
+		{
+			change += cents / coint[i];
+			cents %= coint[i];
+		}
+
+		printf("%d\n", change);
 		return (0);
 	}
-	for (i = 0; i < 5; i++)
-	{
-		change += cents / coint[i];
-		cents %= coint[i];
-	}
-
-	printf("%d\n", change);
 	return (0);
 }
