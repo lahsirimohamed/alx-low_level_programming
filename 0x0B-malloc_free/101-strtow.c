4d04bf0b4e21ad4409498e68067f7762f1451b2e
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-11 06:41:31 -0700

    split string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow- that splits a string into words.
+ * @str: string to split
+ * Return: pointer to the string
+ */
+
+
+char **strtow(char *str)
+{
+	int i;
+	int j;
+	int k = 0;
+	int c = 0;
+	char **array;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	array = malloc (l * sizeof(char *));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] = " " && str[i + 1] != " ")
+		{
+			for (j = i + 1; str[j]; j++)
+			{
+				if (str[j] = " ")
+				{
+					k = j;
+					break;
+				}
+			}
+			array[c] = malloc((k - i) * sizeof(char) + 1);
+			if (array[c] == NULL)
+			{
+				for (j = 0; j < c; j++)
+				{
+					free(array[j]);
+				}
+				free(array);
+				return (NULL);
+			}
+
+			for (j = 0; j < k - i; j++)
+			{
+				array[c][j] = str[j + i];
+			}
+			array[c][k - i] = '\n';
+			c++;
+		}
+	}
+	array[c] = '\n';
+	return (array);
+}
