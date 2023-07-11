bb60c48e4718f6b7215b43bd59e218565beee7a3
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-11 00:50:47 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	int i;
+	char *nstr;
+
+	if (str == NULL)
+		return (NULL);
+	nstr = malloc(sizeof(str));
+	for (i = 0; i < sizeof(str); i++)
+		nstr[i] = str[i];
+	if (nstr == NULL)
+		return (NULL);
+	return(nstr);
+}
