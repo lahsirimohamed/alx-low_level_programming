8bbb6af47e219281cdcb54cfa17faf4ba7678efe
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-13 07:42:03 -0700

    multiply

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main-multiplies two positive numbers.
+ * @argc:number of arg
+ * @argv:array of arg
+ * Return:void no return value
+ */
+
+
+void main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	a = charint(argv[1]);
+	b = charint(argv[2]);
+	printf("%d\n", a * b);
+}
+
+/**
+ * charint-trsf a string to a number
+ * @num: strin of numbers
+ * Return: integer number
+ */
+
+int charint(char *num)
+{
+	int i, a = 0;
+
+	if (num == NULL)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	for (i = 0; num[i]; i++)
+	{
+		if (num[i] >= '0' && num[i] <= '9')
+		{
+			a *= 10;
+			a += num[i] - '0';
+		}
+		else
+		{
+			printf("ERROR\n");
+			exit(98);
+		}
+	}
+	return (a);
+}
+
