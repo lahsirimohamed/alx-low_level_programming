768689431f22b5297d7fba669a0b852076147a40
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-17 12:39:48 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,9 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ndog;
 	char *name1;
 	char *owner1;
-	int l1 = sizelen(name);
-	int l2 = sizelen(owner);
-	int i;
+	int l1 = sizelen(name), l2 = sizelen(owner), i;
+
+	if (name == NULL || owner == NULL || age == 0.0)
+		return (NULL);
 
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
