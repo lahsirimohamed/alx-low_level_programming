b899f13eaa5facc48886b2864906e0273b49db88
Mohamed LAHSIRI <lahsiri.mohamed@gmail.com>
2023-07-20 11:03:24 -0700

    sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,11 +17,14 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_list num;
+
 	va_start(num, n);
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(num, int);
 	}
 	va_end(num);
+
 	return (sum);
 }
