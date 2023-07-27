01013a1cca442f69ef798dd50e45f4246c332ad0
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-27 15:55:17 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
 section .data
-msg_to_print db "Hello, Holberton", 0
+	msg_to_print db "Hello, Holberton", 0
 section .text
-global _start
+	global _start
 
 _start:
-mov rdi, msg_to_print
-call printf
-mov rax, 60
-xor rdi, rdi
-syscall
+	mov rdi, msg_to_print
+	call printf
+	mov rax, 60
+	xor rdi, rdi
+	syscall
