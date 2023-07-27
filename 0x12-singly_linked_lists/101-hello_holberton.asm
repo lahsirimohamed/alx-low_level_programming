7e5cf290af144615ff1d2024c65f905eef40b257
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-27 16:24:31 -0700

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
 global main
 extern printf
 main:
-	mov edi, msg_to_print
+	mov edi, format
 	xor eax, eax
 	call printf
 	mov eax, 0
 	ret
-msg_to_print: db 'Hello, Holberton\n', 0
+format: db `Hello, Holberton\n`,0
