9e397a289142c38a4a4afe004491459b856aad04
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-31 18:07:39 -0700

    add a new node at the begining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,6 +7,7 @@
  * add_nodeint-add new node at the begining
  * @head:pointer to a pointer to a linked list
  * @n:node to add
+ * Return:he address of the new element, or NULL if it failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
