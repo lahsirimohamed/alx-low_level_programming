6b2044ac9e35617facecc50953265224a679d07b
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-08-01 16:42:31 -0700

    fix 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 	if (idx == 0)
 	{
-		new->next = tst;
-		tst = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 	for (i = 0; i < idx && tst; i++)
