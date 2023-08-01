10b80b18ee88eb3f8cb2de6beff1f911a59fcce0
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-07-31 19:37:04 -0700

    the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,22 +13,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if (head == NULL)
-		return (NULL);
 	unsigned int i = 0;
 	listint_t *tst = head;
 
+	if (head == NULL)
+		return (NULL);
 	if (index == 0)
 		return (tst);
 	while (i < index)
 	{
 		if (tst->next == NULL)
 			return (NULL);
-		else
-		{
-			tst = tst->next;
-			i++;
-		}
+		tst = tst->next;
+		i++;
 	}
 	return (tst);
 }
