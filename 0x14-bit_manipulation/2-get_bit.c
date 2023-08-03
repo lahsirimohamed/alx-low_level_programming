222d0745ed09dd1ead22ee1001990d112cf18153
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-08-03 14:01:23 -0700

    fix 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int size = sizeof(unsigned long int) * 8 - 1;
-	int nshifted;
+	unsigned int nshifted;
 
 	if (index > size)
 		return (-1);
