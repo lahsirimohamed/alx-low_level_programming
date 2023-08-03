b4a979c7569071ed307e5b0ec1616ea38696ca3c
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-08-03 13:43:04 -0700

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		sum = 2 * sum + (b[i] - '0');
+		num = 2 * num + (b[i] - '0');
 	}
-	return (sum);
+	return (num);
 }
