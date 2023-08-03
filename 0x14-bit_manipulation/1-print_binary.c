7dd5a9b8730d87156c88b2586276b50c47450ca3
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-08-03 09:45:26 -0700

    the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary-the binary representation of a number
+ * @n:number
+ * Return:void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int size = sizeof(sizeof(unsigned long int)) - 1;
+	int i, flag = 0;
+	unsigned long int nshifted;
+
+	for (i = size; i >= 0; i--)
+	{
+		nshifted = n >> i;
+		if (nshifted & 1)
+		{
+			_putchar('1');
+			flag++;
+		}
+		else if (flag != 0)
+		{
+			_putchar('0');
+		}
+	}
+	if (flag == 0)
+		_putchar('0');
+}
+
