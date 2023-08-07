d6d8a238c3ab3949d7d629d39ee887ea166c711f
lahsirimohamed <lahsiri.mohamed@gmail.com>
2023-08-07 16:45:47 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (size = 0; text_content[size];)
-			size++;
+		for (size = 0; text_content[size]; size++)
+			;
 	}
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	b = write(fd, text_content, len);
+	b = write(a, text_content, size);
 	if (a == -1 || b == -1)
 		return (-1);
 	close(a);
