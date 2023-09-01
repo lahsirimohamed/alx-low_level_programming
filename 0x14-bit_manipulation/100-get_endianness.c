519534e1c25865f944f8d0c7b0bc19c8eec4424b
lahsirimohamed <132397417+lahsirimohamed@users.noreply.github.com>
2023-09-01 02:42:12 +0100

    Add files via upload

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness-checks the endianness.
+ * Return:0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *b = (char *) &n;
+
+	return (*b);
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit-returns the value of a bit at a given index.
+ * @n:number
+ * @index:of the bit
+ * Return:the value of the bit at index index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int size = sizeof(unsigned long int) * 8 - 1;
+	int nshifted;
+
+	if (index > size)
+		return (-1);
+	nshifted = (n >> index) & 1;
+	return (nshifted);
+}
